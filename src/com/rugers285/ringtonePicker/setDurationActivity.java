195620394691adc3b30f79a3a68c5bae7a7e8dd0
rugers285 <justin.ruger@gmail.com>
2011-02-23 11:32:38 -0500

    preference Acting up

--- src/com/rugers285/ringtonePicker/setDurationActivity.java
+++ src/com/rugers285/ringtonePicker/setDurationActivity.java
@@ -8,22 +8,27 @@ import android.preference.PreferenceScreen;
 
 public class setDurationActivity extends PreferenceActivity {
 	final static String SET_DURATION_KEY = "set_duration";
-	
+	boolean change = false;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.sliderpreference);
-        
+        if(change){
+    		finish();
+    		}	
         PreferenceScreen screen = this.getPreferenceScreen();
 
         Preference raction = (Preference) screen.findPreference(SET_DURATION_KEY);
 
     	raction.setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
     	public boolean onPreferenceChange(Preference preference, Object newValue) {
-    	finish();
-    	return true;
+    		change = true;
+    		return true;
     	}
     	});
+    	
+    	
+
                
 	}
 }
