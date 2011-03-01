310b191bef9a23a75167c13b965a53f71145978f
rugers285 <justin.ruger@gmail.com>
2011-03-01 15:34:03 -0500

    .

---
+++ src/com/rugers285/ringtonePicker/clearDialog.java
@@ -0,0 +1,31 @@
+package com.rugers285.ringtonePicker;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.os.Bundle;
+
+public class clearDialog extends Activity {
+	
+	 
+	 
+	        @Override
+	 
+	        public void onCreate(Bundle savedInstanceState) {
+	 
+	                super.onCreate(savedInstanceState);
+	 
+	 
+	 
+	                Dialog dialog = new Dialog(this);
+	 
+	                dialog.setContentView(R.layout.beerwine);
+	 
+	                dialog.setTitle("beer or wine");
+	 
+	                dialog.show();
+	 
+	        }
+	 
+	}
+	
+}
